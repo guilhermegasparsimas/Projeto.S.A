58e51eb93b3bfa0456fbc6a4ec37aded1c27ccef
GUILHERME GASPAR SIMAS <guisimas0201@gmail.com>
2025-05-15 22:16:02 -0300

    15-05

--- Pojeto-S.A/src/App.jsx
+++ Pojeto-S.A/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro'
+import LandingPage from './pages/LandingPage'
 
 function App() {
   const[pagina, setPagina] = useState()
@@ -12,6 +13,7 @@ function App() {
       
       <label className='label-link' onClick={() => setPagina(<Login />)} >Login</label>
       <label className='label-link' onClick={() => setPagina(<Cadastro />)} >Cadastro</label>
+      <label className='label-link' onClick={() => setPagina(<LandingPage />)} >LandingPage</label>
 
     </nav>
     <article>
--- Pojeto-S.A/src/pages/LandingPage.jsx
+++ Pojeto-S.A/src/pages/LandingPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './LandingPage.css'
 
 function LandingPage() {
   return (
     <div className='landing-page'>
-        
+        <figure>
+          <img className='img-landingpage' src="/img/bergamota.png" alt="" />
+          <figcaption>Imagem de uma Bergamota</figcaption>
+        </figure>
       
     </div>
   )
