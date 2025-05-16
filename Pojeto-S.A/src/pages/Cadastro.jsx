38ecbd181cd52ec2d5a9bca92a19a863dc30fc66
GUILHERME GASPAR SIMAS <guisimas0201@gmail.com>
2025-05-15 21:41:17 -0300

    OIII

--- Pojeto-S.A/src/pages/Cadastro.jsx
+++ Pojeto-S.A/src/pages/Cadastro.jsx
@@ -9,7 +9,7 @@ function Cadastro() {
         <input type="text"  id="NameCliente" placeholder='Digite o nome de usuário'/>
       <label htmlFor="">Digite seu e-mail </label>
       <input type="Email"  id="Email" placeholder='Digite seu e-mail' />
-        <label >Senha</label>
+        <label >Digite sua senha</label>
         <input type="Password"  id="Password" placeholder='Digite sua senha' />
         <button className='btn-cad'>Cadastrar</button>
       </div>
---
+++ Pojeto-S.A/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='landing-page'>
+        
+      
+    </div>
+  )
+}
+
+export default LandingPage
